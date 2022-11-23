3c3785ad5980a7e439b91d31796b7ea87bf26a33
Wahab Ahmad <w28ahmad@uwaterloo.ca>
2022-11-23 01:01:26 -0500

    Working mysql dockerfile

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@ export $(shell sed 's/=.*//' .env)
 MULA_MYSQL_IP := $(shell docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mula-mysql)
 
 mysql-container:
-	docker run --name mula-mysql -e MYSQL_ROOT_PASSWORD=${DB_PASSWORD} -d mysql:latest
+	docker run --name mula-mysql -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} -d mysql:latest
 
 mysql-connect:
 	docker run -it --network bridge --rm mysql mysql -h $(MULA_MYSQL_IP) -u ${DB_USER} -p
--- refreshDB.sh
+++ refreshDB.sh
@@ -7,13 +7,14 @@ if [[ -z "${DB_HOST}" ]]; then
     echo "[Error] DB_HOST not set" 1>&2
 elif [[ -z "${DB_USER}" ]]; then
     echo "[Error] DB_USER not set" 1>&2
-elif [[ -z "${DB_PASSWORD}" ]]; then
-    echo "[Error] DB_PASSWORD not set" 1>&2
+elif [[ -z "${MYSQL_ROOT_PASSWORD}" ]]; then
+    echo "[Error] MYSQL_ROOT_PASSWORD not set" 1>&2
 else
     echo '[LOG] Creating DB'
-    docker exec -i mula-mysql mysql --password=${DB_PASSWORD} --user=${DB_USER} < createQuestionStore.sql 2>&1
+    # docker exec -i mula-mysql mysql --password=${MYSQL_ROOT_PASSWORD} --user=${DB_USER} < createQuestionStore.sql 2>&1
+    mysql --password=${MYSQL_ROOT_PASSWORD} --user=${DB_USER} < createQuestionStore.sql 2>&1
     
     echo '[LOG] Migrating Data'
-    docker exec -i mula-mysql mysql --password=${DB_PASSWORD} --user=${DB_USER} < insertQuestionStore.sql 2>&1
+    # docker exec -i mula-mysql mysql --password=${MYSQL_ROOT_PASSWORD} --user=${DB_USER} < insertQuestionStore.sql 2>&1
+    mysql --password=${MYSQL_ROOT_PASSWORD} --user=${DB_USER} < insertQuestionStore.sql 2>&1
 fi
-
