aa6e582da32ef55e02e21fd4bee910c4bd8880b1
Wahab Ahmad <w28ahmad@uwaterloo.ca>
2022-11-23 23:09:46 -0500

    Adding prod db

--- Makefile
+++ Makefile
@@ -3,12 +3,18 @@ include .env
 
 export $(shell sed 's/=.*//' .env)
 
-MULA_MYSQL_IP := $(shell docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mula-mysql)
+# MULA_MYSQL_IP := $(shell docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mula-mysql)
 
-mysql-container:
+mysql-container-docker:
 	docker run --name mula-mysql -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} -d mysql:latest
 
-mysql-connect:
-	docker run -it --network bridge --rm mysql mysql -h $(MULA_MYSQL_IP) -u ${DB_USER} -p
+# mysql-connect-docker:
+# docker run -it --network bridge --rm mysql mysql -h $(MULA_MYSQL_IP) -u ${DB_USER} -p
 
-.PHONY: mysql-container mysql-connect
+mysql-connect-rds:
+	mysql -h ${RDS_HOST} -u ${DB_USER} --password=${RDS_PASSWORD}
+
+mysql-rds-migrate:
+	./refresh.sh --prod
+
+.PHONY: mysql-container-docker mysql-connect-rds
--- refreshDB.sh
+++ refreshDB.sh
@@ -3,18 +3,34 @@ source '.env'
 echo '[LOG] Creation insertion script'
 python3 generateInsertQuery.py 
 
-if [[ -z "${DB_HOST}" ]]; then
-    echo "[Error] DB_HOST not set" 1>&2
-elif [[ -z "${DB_USER}" ]]; then
-    echo "[Error] DB_USER not set" 1>&2
-elif [[ -z "${MYSQL_ROOT_PASSWORD}" ]]; then
-    echo "[Error] MYSQL_ROOT_PASSWORD not set" 1>&2
+if [[ $1 == "--prod" ]]; then
+    if [[ -z "${RDS_HOST}" ]]; then
+        echo "[Error] RDS_HOST not set" 1>&2
+    elif [[ -z "${DB_USER}" ]]; then
+        echo "[Error] DB_USER not set" 1>&2
+    elif [[ -z "${RDS_PASSWORD}" ]]; then
+        echo "[Error] RDS_PASSWORD not set" 1>&2
+    else
+        echo '[LOG] Creating DB'
+        mysql -h ${RDS_HOST} --password=${RDS_PASSWORD} --user=${DB_USER} < createQuestionStore.sql 2>&1
+        
+        echo '[LOG] Migrating Data'
+        mysql -h ${RDS_HOST} --password=${RDS_PASSWORD} --user=${DB_USER} < insertQuestionStore.sql 2>&1
+    fi
 else
-    echo '[LOG] Creating DB'
-    # docker exec -i mula-mysql mysql --password=${MYSQL_ROOT_PASSWORD} --user=${DB_USER} < createQuestionStore.sql 2>&1
-    mysql --password=${MYSQL_ROOT_PASSWORD} --user=${DB_USER} < createQuestionStore.sql 2>&1
-    
-    echo '[LOG] Migrating Data'
-    # docker exec -i mula-mysql mysql --password=${MYSQL_ROOT_PASSWORD} --user=${DB_USER} < insertQuestionStore.sql 2>&1
-    mysql --password=${MYSQL_ROOT_PASSWORD} --user=${DB_USER} < insertQuestionStore.sql 2>&1
+    if [[ -z "${DB_HOST}" ]]; then
+        echo "[Error] DB_HOST not set" 1>&2
+    elif [[ -z "${DB_USER}" ]]; then
+        echo "[Error] DB_USER not set" 1>&2
+    elif [[ -z "${MYSQL_ROOT_PASSWORD}" ]]; then
+        echo "[Error] MYSQL_ROOT_PASSWORD not set" 1>&2
+    else
+        echo '[LOG] Creating DB'
+        # docker exec -i mula-mysql mysql --password=${MYSQL_ROOT_PASSWORD} --user=${DB_USER} < createQuestionStore.sql 2>&1
+        mysql --password=${MYSQL_ROOT_PASSWORD} --user=${DB_USER} < createQuestionStore.sql 2>&1
+        
+        echo '[LOG] Migrating Data'
+        # docker exec -i mula-mysql mysql --password=${MYSQL_ROOT_PASSWORD} --user=${DB_USER} < insertQuestionStore.sql 2>&1
+        mysql --password=${MYSQL_ROOT_PASSWORD} --user=${DB_USER} < insertQuestionStore.sql 2>&1
+    fi
 fi
